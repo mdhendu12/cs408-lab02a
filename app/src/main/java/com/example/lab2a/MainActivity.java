c9d322870a85a3df702d89585f45b9427cfc6f9b
matthew <mdhendu12@gmail.com>
2023-02-02 17:16:14 -0600

    Program seems to be finished, waiting on professor feedback.

--- app/src/main/java/com/example/lab2a/MainActivity.java
+++ app/src/main/java/com/example/lab2a/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.lab2a;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.lab2a.databinding.ActivityMainBinding;
+
+import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+    public void onClick(View view) {
+        BigDecimal bill = new BigDecimal(binding.editTotalBill.getText().toString());
+        BigDecimal tip = new BigDecimal(binding.editTipPercentage.getText().toString());
+        BigDecimal numOfPeople = new BigDecimal(binding.editNumOfPeople.getText().toString());
+
+        String tpp = "Total Per Person: $" + TipCalculator.calculateTip(bill, tip, numOfPeople);
+
+        binding.totalPerPerson.setText(tpp);
     }
 }
